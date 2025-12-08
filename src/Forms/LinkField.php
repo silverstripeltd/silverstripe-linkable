70f5873be076116986c1bd63b9e57d1eede9f909
Ishan Jayamannne <ishanjmails@gmail.com>
2025-12-07 20:18:46 -0600

    update FormField::Value() to getValue()

--- src/Forms/LinkField.php
+++ src/Forms/LinkField.php
@@ -153,12 +153,12 @@ class LinkField extends TextField
     {
         $requestID = Controller::curr()->request->requestVar('LinkID');
 
-        if ($requestID == '0' && !$this->Value()) {
+        if ($requestID == '0' && !$this->getValue()) {
             return null;
         }
 
         if (!$this->linkObject) {
-            $id = $this->Value() ? $this->Value() : $requestID;
+            $id = $this->getValue() ? $this->getValue() : $requestID;
 
             if ((int)$id) {
                 $this->linkObject = Link::get()->byID($id);
